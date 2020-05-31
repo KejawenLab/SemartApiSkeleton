77e24987d3a4d6df373ad681cdf572b8f6a8ff11
Muhamad Surya Iksanudin <surya.kejawen@gmail.com>
2020-06-01 00:05:39 +0700

    improve generator

--- src/Command/GenerateCommand.php
+++ src/Command/GenerateCommand.php
@@ -9,7 +9,9 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 /**
  * @author Muhamad Surya Iksanudin<surya.iksanudin@alpabit.com>
@@ -35,11 +37,31 @@ final class GenerateCommand extends Command
             ->setDescription('Generate RESTful API')
             ->setDescription('Generate RESTful API')
             ->addArgument('entity', InputArgument::REQUIRED)
+            ->addOption('parent', 'p', InputOption::VALUE_REQUIRED)
+            ->addOption('force', 'f', InputOption::VALUE_NONE)
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $output->write('<fg=green;options=bold>
+   _____                           __     ______                           __
+  / ___/___  ____ ___  ____ ______/ /_   / ____/__  ____  ___  _________ _/ /_____  _____
+  \__ \/ _ \/ __ `__ \/ __ `/ ___/ __/  / / __/ _ \/ __ \/ _ \/ ___/ __ `/ __/ __ \/ ___/
+ ___/ /  __/ / / / / / /_/ / /  / /_   / /_/ /  __/ / / /  __/ /  / /_/ / /_/ /_/ / /
+/____/\___/_/ /_/ /_/\__,_/_/   \__/   \____/\___/_/ /_/\___/_/   \__,_/\__/\____/_/
+
+<comment>By: KejawenLab - Muhamad Surya Iksanudin<surya.kejawen@gmail.com></comment>
+
+</>');
+        if (!$input->getOption('force')) {
+            $helper = $this->getHelper('question');
+            $question = new ConfirmationQuestion('<comment>[!!!WARNING!!!]</comment><question> Semart Api Generator will override your file if it exists. Continue?</question> (y/n)', false);
+            if (!$helper->ask($input, $output, $question)) {
+                return 0;
+            }
+        }
+
         /** @var string $entity */
         $entity = $input->getArgument('entity');
         $reflection = new \ReflectionClass(sprintf('%s\%s', self::NAMESPACE, $entity));
@@ -54,17 +76,17 @@ final class GenerateCommand extends Command
             '--no-interaction' => null,
         ]), $output);
 
-        $output->writeln('<info>Running Semart Schema Updater</info>');
+        $output->writeln('<info>Running Schema Updater</info>');
         $update = $application->find('doctrine:migration:migrate');
         $update->run(new ArrayInput([
             'command' => 'doctrine:migration:migrate',
             '--no-interaction' => null,
         ]), $output);
 
-        $output->writeln('<info>Running Semart RESTful API Generator</info>');
+        $output->writeln('<info>Generating RESTful API</info>');
         $this->generatorFactory->generate($reflection);
 
-        $output->writeln(sprintf('<comment>Simple RESTful for %s class is generated</comment>', $reflection->getName()));
+        $output->writeln(sprintf('<comment>RESTful API for <info>"%s"</info> class is generated</comment>', $reflection->getName()));
 
         return 0;
     }
