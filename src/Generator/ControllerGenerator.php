f2ac1355f98231e22bc935acceb292f2e4dfbb2f
Muhamad Surya Iksanudin <surya.kejawen@gmail.com>
2020-07-18 11:55:08 +0700

    improve controller generator. skip create audit controller when entity is not auditable

--- src/Generator/ControllerGenerator.php
+++ src/Generator/ControllerGenerator.php
@@ -4,13 +4,27 @@ declare(strict_types=1);
 
 namespace KejawenLab\ApiSkeleton\Generator;
 
+use DH\DoctrineAuditBundle\Annotation\Auditable;
+use Doctrine\Common\Annotations\Reader;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Twig\Environment;
 
 /**
  * @author Muhamad Surya Iksanudin<surya.kejawen@gmail.com>
  */
 final class ControllerGenerator extends AbstractGenerator
 {
+    private Reader $reader;
+
+    public function __construct(Reader $reader, Environment $twig, Filesystem $fileSystem, KernelInterface $kernel)
+    {
+        parent::__construct($twig, $fileSystem, $kernel);
+
+        $this->reader = $reader;
+    }
+
     public function generate(\ReflectionClass $class, OutputInterface $output): void
     {
         $shortName = $class->getShortName();
@@ -19,7 +33,6 @@ final class ControllerGenerator extends AbstractGenerator
         $delete = $this->twig->render('generator/delete.php.twig', ['entity' => $shortName]);
         $post = $this->twig->render('generator/post.php.twig', ['entity' => $shortName]);
         $put = $this->twig->render('generator/put.php.twig', ['entity' => $shortName]);
-        $audit = $this->twig->render('generator/audit.php.twig', ['entity' => $shortName]);
 
         $output->writeln(sprintf('<comment>Generating class <info>"KejawenLab\ApiSkeleton\Controller\%s\GetAll"</info></comment>', $shortName));
         $this->fileSystem->dumpFile(sprintf('%s/src/Controller/%s/GetAll.php', $this->kernel->getProjectDir(), $shortName), $getAll);
@@ -31,7 +44,11 @@ final class ControllerGenerator extends AbstractGenerator
         $this->fileSystem->dumpFile(sprintf('%s/src/Controller/%s/Post.php', $this->kernel->getProjectDir(), $shortName), $post);
         $output->writeln(sprintf('<comment>Generating class <info>"KejawenLab\ApiSkeleton\Controller\%s\Put"</info></comment>', $shortName));
         $this->fileSystem->dumpFile(sprintf('%s/src/Controller/%s/Put.php', $this->kernel->getProjectDir(), $shortName), $put);
-        $output->writeln(sprintf('<comment>Generating class <info>"KejawenLab\ApiSkeleton\Controller\%s\Audit"</info></comment>', $shortName));
-        $this->fileSystem->dumpFile(sprintf('%s/src/Controller/%s/Audit.php', $this->kernel->getProjectDir(), $shortName), $audit);
+
+        if ($this->reader->getClassAnnotation($class, Auditable::class)) {
+            $audit = $this->twig->render('generator/audit.php.twig', ['entity' => $shortName]);
+            $output->writeln(sprintf('<comment>Generating class <info>"KejawenLab\ApiSkeleton\Controller\%s\Audit"</info></comment>', $shortName));
+            $this->fileSystem->dumpFile(sprintf('%s/src/Controller/%s/Audit.php', $this->kernel->getProjectDir(), $shortName), $audit);
+        }
     }
 }
