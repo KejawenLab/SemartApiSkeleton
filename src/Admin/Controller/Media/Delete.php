1301c435bea08fe46cd9c64038f5635e9bd1252f
Muhamad Surya Iksanudin <surya.kejawen@gmail.com>
2020-07-30 22:36:31 +0700

    improve admin crud

---
+++ src/Admin/Controller/Media/Delete.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KejawenLab\ApiSkeleton\Admin\Controller\Media;
+
+use KejawenLab\ApiSkeleton\Media\MediaService;
+use KejawenLab\ApiSkeleton\Media\Model\MediaInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @author Muhamad Surya Iksanudin<surya.kejawen@gmail.com>
+ */
+final class Delete extends AbstractController
+{
+    private MediaService $service;
+
+    public function __construct(MediaService $service)
+    {
+        $this->service = $service;
+    }
+
+    /**
+     * @Route("/medias/{id}/delete", methods={"GET"})
+     */
+    public function __invoke(Request $request, string $id)
+    {
+        $media = $this->service->get($id);
+        if (!$media instanceof MediaInterface) {
+            $this->addFlash('error', 'sas.page.media.not_found');
+
+            return new RedirectResponse($this->generateUrl('kejawenlab_apiskeleton_admin_media_getall__invoke'));
+        }
+
+        $this->service->remove($media);
+
+        $this->addFlash('info', 'sas.page.media.deleted');
+
+        return new RedirectResponse($this->generateUrl('kejawenlab_apiskeleton_admin_media_getall__invoke'));
+    }
+}
---
+++ src/Form/MediaType.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KejawenLab\ApiSkeleton\Form;
+
+use KejawenLab\ApiSkeleton\Entity\Group;
+use KejawenLab\ApiSkeleton\Entity\Media;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * @author Muhamad Surya Iksanudin<surya.kejawen@gmail.com>
+ */
+final class MediaType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('file', FileType::class, [
+            'required' => true,
+            'label' => 'sas.form.field.file',
+        ]);
+        $builder->add('folder', TextType::class, [
+            'required' => true,
+            'label' => 'sas.form.field.folder',
+        ]);
+        $builder->add('public', CheckboxType::class, [
+            'required' => false,
+            'label' => 'sas.form.field.public',
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Media::class,
+        ]);
+    }
+}
