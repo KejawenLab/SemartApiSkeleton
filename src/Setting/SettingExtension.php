17b7b0b1b0212e9a44a7b78f31f1b48091c70c34
Muhamad Surya Iksanudin <surya.kejawen@gmail.com>
2020-10-09 00:56:48 +0700

    add setting extension

---
+++ src/Setting/SettingExtension.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KejawenLab\ApiSkeleton\Setting;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+/**
+ * @author Muhamad Surya Iksanudin<surya.kejawen@gmail.com>
+ */
+class SettingExtension extends AbstractExtension
+{
+    private SettingService $service;
+
+    public function __construct(SettingService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function getSetting(string $key)
+    {
+        return $this->service->getSetting($key);
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('setting', [$this, 'getSetting']),
+        ];
+    }
+}
