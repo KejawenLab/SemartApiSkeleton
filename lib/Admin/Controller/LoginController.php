e9f92df0303628a502c408b90fe177f2e0f1f445
ad3n <surya.kejawen@gmail.com>
2021-03-23 23:26:24 +0700

    fix error security

--- lib/Admin/Controller/LoginController.php
+++ lib/Admin/Controller/LoginController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 /**
  * @author Muhamad Surya Iksanudin<surya.kejawen@gmail.com>
@@ -19,12 +20,18 @@ final class LoginController extends AbstractController
     /**
      * @Route("/login", name=LoginController::ROUTE_NAME, methods={"GET", "POST"})
      */
-    public function __invoke(Request $request): Response
+    public function __invoke(Request $request, AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser()) {
             return $this->redirectToRoute('admin_home');
         }
 
-        return $this->render('layout/login.html.twig');
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('layout/login.html.twig', [
+            'error' => $error,
+            'username' => $lastUsername,
+        ]);
     }
 }
