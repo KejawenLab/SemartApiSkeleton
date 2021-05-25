6ed468af6069800518d46244365b6040129823a0
ad3n <surya.kejawen@gmail.com>
2021-05-25 10:30:22 +0700

    fix version

--- lib/SemartApiSkeleton.php
+++ lib/SemartApiSkeleton.php
@@ -11,13 +11,13 @@ final class SemartApiSkeleton
 {
     public const CODENAME = 'Apem Comal';
 
-    public const VERSION = '5.3.0';
+    public const VERSION = '5.3.3';
 
     public const VERSION_MAYOR = 50000;
 
     public const VERSION_MINOR = 3;
 
-    public const VERSION_PATCH = 0;
+    public const VERSION_PATCH = 3;
 
     public static function getVersionNumber(): int
     {
