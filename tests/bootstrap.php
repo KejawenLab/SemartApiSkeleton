f20a366f3defd96b7fd3b2cad0924b24acfd0b0f
ad3n <surya.kejawen@gmail.com>
2021-06-15 22:15:54 +0700

    add more test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,9 @@
 <?php
 
+use DG\BypassFinals;
 use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
-    require dirname(__DIR__).'/config/bootstrap.php';
-} elseif (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-}
+(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+BypassFinals::enable();
