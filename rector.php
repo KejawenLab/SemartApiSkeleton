260c5dfae57d690f6a73954c2746660608171ad0
ad3n <surya.kejawen@gmail.com>
2021-06-15 11:49:05 +0700

    improve code

--- rector.php
+++ rector.php
@@ -8,19 +8,13 @@ use Rector\Symfony\Rector\Attribute\ExtractAttributeRouteNameConstantsRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    // get parameters
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::AUTO_IMPORT_NAMES, true);
 
-    // Define what rule sets will be applied
     $containerConfigurator->import(SetList::DEAD_CODE);
 
-    // get services (needed for register a single rule)
     $services = $containerConfigurator->services();
     $services->set(ExtractAttributeRouteNameConstantsRector::class);
 
     $containerConfigurator->import(SetList::PHP_80);
-
-    // register a single rule
-    // $services->set(TypedPropertyRector::class);
 };
