0716dcccdf801bd555494fab9d0257948cf180ff
ad3n <surya.kejawen@gmail.com>
2021-06-25 16:14:30 +0700

    prepare symfony runtime

--- public/index.php
+++ public/index.php
@@ -1,15 +1,23 @@
 <?php
 
-use KejawenLab\ApiSkeleton\CachedKernel;
 use KejawenLab\ApiSkeleton\Kernel;
+use Swoole\Constant;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+$root = dirname(__DIR__);
+
+$_SERVER['APP_RUNTIME_OPTIONS'] = [
+    'host' => '0.0.0.0',
+    'port' => 9501,
+    'mode' => SWOOLE_BASE,
+    'settings' => [
+        Constant::OPTION_WORKER_NUM => swoole_cpu_num() * 2,
+        Constant::OPTION_ENABLE_STATIC_HANDLER => true,
+        Constant::OPTION_DOCUMENT_ROOT => sprintf('%s/%s', $root, 'public')
+    ],
+];
+
+require_once sprintf('%s/%s', $root, 'vendor/autoload_runtime.php');
 
 return function (array $context) {
-    $kernel = new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-    if ('prod' === $kernel->getEnvironment()) {
-        $kernel = new CachedKernel($kernel);
-    }
-
-    return $kernel;
+    return new Kernel($context['APP_ENV'], $context['APP_DEBUG']);
 };
