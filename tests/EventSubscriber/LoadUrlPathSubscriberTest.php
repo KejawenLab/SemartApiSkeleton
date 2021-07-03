fa52d9e1b75d772e464e987ef9f0c2cf5c0e8ee0
ad3n <surya.kejawen@gmail.com>
2021-07-03 18:20:42 +0700

    add more test

--- tests/EventSubscriber/LoadUrlPathSubscriberTest.php
+++ tests/EventSubscriber/LoadUrlPathSubscriberTest.php
@@ -6,8 +6,10 @@ namespace KejawenLab\ApiSkeleton\Tests\EventSubscriber;
 
 use Doctrine\ORM\Event\LifecycleEventArgs;
 use Doctrine\ORM\Events;
+use KejawenLab\ApiSkeleton\Entity\Menu;
 use KejawenLab\ApiSkeleton\EventSubscriber\LoadUrlPathSubscriber;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Routing\Exception\RouteNotFoundException;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 /**
@@ -24,6 +26,41 @@ class LoadUrlPathSubscriberTest extends TestCase
         $subscriber = new LoadUrlPathSubscriber($urlGenerator);
 
         $subscriber->postLoad($event);
+    }
+
+    public function testIsAdminOnly(): void
+    {
+        $menu = new Menu();
+        $menu->setAdminOnly(true);
+        $menu->setRouteName('#');
+
+        $event = $this->createMock(LifecycleEventArgs::class);
+        $event->expects($this->once())->method('getObject')->willReturn($menu);
+
+        $urlGenerator = $this->createMock(UrlGeneratorInterface::class);
+        $urlGenerator->expects($this->never())->method('generate');
+
+        $subscriber = new LoadUrlPathSubscriber($urlGenerator);
+
+        $subscriber->postLoad($event);
+    }
+
+    public function testNotFoundRoute(): void
+    {
+        $menu = new Menu();
+        $menu->setAdminOnly(true);
+        $menu->setRouteName('fake_route');
+
+        $event = $this->createMock(LifecycleEventArgs::class);
+        $event->expects($this->once())->method('getObject')->willReturn($menu);
+
+        $urlGenerator = $this->createMock(UrlGeneratorInterface::class);
+        $urlGenerator->expects($this->once())->method('generate')->willThrowException(new RouteNotFoundException());
+
+        $subscriber = new LoadUrlPathSubscriber($urlGenerator);
+
+        $subscriber->postLoad($event);
+        $this->assertSame('#', $menu->getAdminPath());
     }
 
     public function testGetEventClass(): void
