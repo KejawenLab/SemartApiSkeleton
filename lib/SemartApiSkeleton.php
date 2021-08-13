3d6fc6e399bbd840468c2cc70998e0993302af9f
Muhamad Surya Iksanudin <surya.kejawen@gmail.com>
2021-08-13 22:45:37 +0700

    update version

--- lib/SemartApiSkeleton.php
+++ lib/SemartApiSkeleton.php
@@ -11,13 +11,13 @@ final class SemartApiSkeleton
 {
     public const CODENAME = 'Apem Comal';
 
-    public const VERSION = '5.4.4';
+    public const VERSION = '5.4.5';
 
     public const VERSION_MAYOR = 50000;
 
     public const VERSION_MINOR = 400;
 
-    public const VERSION_PATCH = 4;
+    public const VERSION_PATCH = 5;
 
     public static function getVersionNumber(): int
     {
