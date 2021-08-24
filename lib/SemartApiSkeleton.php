56b793c834184a12db0c5f462ed63c97a8b17b2e
Muhamad Surya Iksanudin <surya.kejawen@gmail.com>
2021-08-24 18:15:16 +0700

    fix version

--- lib/SemartApiSkeleton.php
+++ lib/SemartApiSkeleton.php
@@ -11,13 +11,13 @@ final class SemartApiSkeleton
 {
     public const CODENAME = 'Merdeka';
 
-    public const VERSION = '5.6.1';
+    public const VERSION = '5.6.3';
 
     public const VERSION_MAYOR = 50000;
 
     public const VERSION_MINOR = 600;
 
-    public const VERSION_PATCH = 2;
+    public const VERSION_PATCH = 3;
 
     public static function getVersionNumber(): int
     {
