e4f36c1fe4e849227e3174b216c6db46ae1bb603
Muhamad Surya Iksanudin <surya.kejawen@gmail.com>
2022-01-15 14:13:39 +0700

    fix error generator

--- lib/SemartApiSkeleton.php
+++ lib/SemartApiSkeleton.php
@@ -11,13 +11,13 @@ final class SemartApiSkeleton
 {
     final public const CODENAME = 'Dodol Duren';
 
-    final public const VERSION = '5.7.5';
+    final public const VERSION = '5.7.6';
 
     final public const VERSION_MAYOR = 50000;
 
     final public const VERSION_MINOR = 700;
 
-    final public const VERSION_PATCH = 5;
+    final public const VERSION_PATCH = 6;
 
     public static function getVersionNumber(): int
     {
